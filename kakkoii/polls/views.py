8379a92b23e6f963c69c05b2f0aa5d72a0fa5be1
Tom-2013-06417 <airbreather7@gmail.com>
2014-07-03 00:10:10 +0800

    Finished with Tutorial part 3

--- kakkoii/polls/views.py
+++ kakkoii/polls/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.template import RequestContext, loader
+
+from polls.models import Poll
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the poll index.")
+    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+    context = {'latest_poll_list': latest_poll_list}
+    return render(request, 'polls/index.html', context)
 
 def detail(request, poll_id):
-    return HttpResponse("You're looking at poll %s." % poll_id)
+    try:
+        poll = Poll.objects.get(pk=poll_id)
+    except Poll.DoesNotExist:
+        raise Http404
+    return render(request, 'polls/detail.html', {'poll': poll})
 
 def results(request, poll_id):
     return HttpResponse("You're looking at the results of poll %s." % poll_id)
